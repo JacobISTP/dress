b1775cfce7c202f1d1c34715a70e0ee0e192bd5f
jangbly62 <wkdgur112@gmail.com>
2022-03-31 12:09:11 +0900

    data insert

---
+++ main.js
@@ -0,0 +1,169 @@
+const dressshop = [
+  {
+    data: [
+      "벨라스포사",
+      99,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "신상드레스+서비스, 피팅비 지원 서비스",
+    ],
+    phone: "0507-1401-5312",
+    address: "충남 천안시 서북구 백석로 207",
+    worktime: "10:00~19:00 화 휴무",
+    page: "http://blog.naver.com/oj0508",
+    instagram: "",
+  },
+  {
+    data: [
+      "쥬디",
+      104,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "신상드레스+서비스, 피팅비 지원 서비스",
+    ],
+    phone: "041-574-5841",
+    address: "충남 천안시 서북구 미라14길 4",
+    worktime: "",
+    page: "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EC%A5%AC%EB%94%94%EC%9B%A8%EB%94%A9&oquery=%EC%A5%AC%EB%94%94&tqi=hCRSBdprvhGssAfoKNZssssss5l-161853",
+    instagram: "",
+  },
+  {
+    data: [
+      "뮤즈앤코",
+      113,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "신상드레스+서비스, 피팅비 지원 서비스",
+    ],
+    phone: "0507-1346-2842",
+    address: "충남 천안시 동남구 쌍용대로 179 1층 뮤즈앤코",
+    worktime: "09:30~19:00 월화 휴무",
+    page: "http://ninemusestudio.com/musenco",
+    instagram: "https://www.instagram.com/muse_n_co/",
+  },
+  {
+    data: [
+      "JM",
+      115,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "피팅비 지원 서비스",
+    ],
+    phone: "0507-1412-5673",
+    address: "충남 천안시 동남구 청수12로 34",
+    worktime: "11:00~21:00 휴무X",
+    page: "https://www.facebook.com/jisung.kim.3910",
+    instagram: "https://www.instagram.com/jm.dress/",
+  },
+
+  {
+    data: [
+      "브리예",
+      140,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "신상드레스+서비스, 피팅비 지원 서비스",
+    ],
+    phone: "",
+    address: "충남 천안시 서북구 성정중4길 39-3",
+    worktime: "",
+    page: "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%B8%8C%EB%A6%AC%EC%98%88",
+    instagram: "",
+  },
+  {
+    data: [
+      "르네블랑",
+      120,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "신상드레스+서비스, 피팅비 지원 서비스",
+    ],
+    phone: "041-909-0113",
+    address: "충남 천안시 서북구 불당32길 7 1층",
+    worktime: "10:00~19:00 휴무X",
+    page: "https://blog.naver.com/NBlogTop.naver?isHttpsRedirect=true&blogId=ameliewedd",
+    instagram: "https://www.instagram.com/leneblanc/",
+  },
+  {
+    data: [
+      "마샬브라이드",
+      120,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "피팅비 지원 서비스",
+    ],
+    phone: "0507-1465-8691",
+    address: "충남 천안시 서북구 불당29길 19",
+    worktime: "10:00~18:00 휴무X",
+    page: "https://marshallbride.creatorlink.net/",
+    instagram:
+      "https://www.instagram.com/accounts/login/?next=/marshallbride_cheonan/",
+  },
+  {
+    data: [
+      "아델라브라이드",
+      130,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "피팅비 지원 서비스",
+    ],
+    phone: "043-715-6030",
+    address: "충북 청주시 흥덕구 대농로 70 2층 203호",
+    worktime: "09:30~18:30 12:00~13:00휴게시간 화 휴무",
+    page: "https://blog.naver.com/promakeup11",
+    instagram:
+      "https://www.instagram.com/accounts/login/?next=/adelabride_official/",
+  },
+  {
+    data: [
+      "마벨르로베",
+      120,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "피팅비 지원 서비스",
+    ],
+    phone: "0507-1493-7902",
+    address: "충북 청주시 상당구 남사로 97-1 1층 마벨르로베",
+    worktime: "10:30~18:00 월화 휴무",
+    page: "https://blog.naver.com/NBlogTop.naver?isHttpsRedirect=true&blogId=aka0319",
+    instagram: "",
+  },
+  {
+    data: [
+      "하우스오브에이미",
+      245,
+      "드레스1벌, 턱시도1벌 \n 헤어&메이크업(신랑,신부)",
+      "",
+    ],
+    phone: "0507-1377-8576",
+    address: "서울 강남구 선릉로152길 25 위빌딩 2,3층",
+    worktime: "10:00~20:00 일월 휴무",
+    page: "http://houseofamy.com/",
+    instagram:
+      "https://www.instagram.com/accounts/login/?next=/houseofamy_official/",
+  },
+];
+const makeupshop = [
+  {
+    data: [
+      "손수",
+      "손수는 투명메이크업으로 유명합니다. 손수만 원장님 부원장님 지정시 추가비가 발생이들어갑니다. 현재 지정예약은 받지않고 있습니다.ㅠㅠ",
+    ],
+    phone: "0507-1308-4886",
+    address: "충북 청주시 흥덕구 강서로 111",
+    worktime: "05:00~19:00 휴무X",
+    page: "https://blog.naver.com/NBlogTop.naver?isHttpsRedirect=true&blogId=doadoa77",
+    instagram:
+      "https://www.instagram.com/accounts/login/?next=/sonsoo.makeup_official/",
+  },
+  {
+    data: ["바이윤", "바이윤은 헤어로 유명하구요~"],
+    phone: "010-3186-1527",
+    address: "충북 청주시 서원구 용호로 66",
+    worktime: "09:00~20:00(상담) 월화 휴무",
+    page: "https://blog.naver.com/yoon-1213/221527238848",
+    instagram: "https://www.instagram.com/_by__yoon/",
+  },
+  {
+    data: [
+      "라보떼",
+      "라보떼는 교수님께서 직접 메이크업을 해주시기때문에 신부님께서 만족도가 높습니다.^^",
+    ],
+    phone: "043-231-7757",
+    address: "충북 청주시 서원구 장전로12번길 12 2F",
+    worktime: "06:00~18:00 화 휴무",
+    page: "https://www.instagram.com/la_beaute_beauty_house/",
+    instagram: "https://www.instagram.com/la_beaute_beauty_house/",
+  },
+];
